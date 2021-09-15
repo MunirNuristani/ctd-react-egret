763bd1765491a0b38ee45ecc0d999f2d5f3eaf24
MunirNuristani <nuristani.munir@gmail.com>
2021-09-14 18:15:10 -0700

    Completed the lesson-1-4.

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,20 +1,6 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
-const todoList = [
-    {
-      id: 1,
-      title: "Fork the Assignment"
-    },
-    {
-      id: 2,
-      title: "Complete the Assignment"
-    },
-    {
-      id:3, 
-      title: "Submit the Assignment"
-    }
-  ]
-function TodoList(){
+function TodoList({ todoList }){
     return(
         <ul>
             {todoList.map(function(item){
